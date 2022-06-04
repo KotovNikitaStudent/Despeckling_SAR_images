24233bca58aa3fbb6c8c93c717fea203a69ffe93
Nikita <nikita@MacBook-Air-Nikita.local>
2022-06-04 16:52:32 +0300

    upgrade model, refresh readme

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
 # Despeckling_SAR_images
 Repository contains an algorithm that removes speckle noise from SAR images.
 The algorithm based on using convolutional neural network. Using PyTorch framework.
+
+# Description of CNN
+Despeckle filter consist from convolution layers (CNN).
+Summary:
+```
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+            Conv2d-1         [-1, 64, 256, 256]           1,792
+              conv-2         [-1, 64, 256, 256]               0
+         LeakyReLU-3         [-1, 64, 256, 256]               0
+            Conv2d-4         [-1, 64, 256, 256]          36,928
+           conv_dr-5         [-1, 64, 256, 256]               0
+       BatchNorm2d-6         [-1, 64, 256, 256]             128
+         LeakyReLU-7         [-1, 64, 256, 256]               0
+            Conv2d-8         [-1, 64, 256, 256]          36,928
+           conv_dr-9         [-1, 64, 256, 256]               0
+      BatchNorm2d-10         [-1, 64, 256, 256]             128
+        LeakyReLU-11         [-1, 64, 256, 256]               0
+           Conv2d-12         [-1, 64, 256, 256]          36,928
+          conv_dr-13         [-1, 64, 256, 256]               0
+      BatchNorm2d-14         [-1, 64, 256, 256]             128
+        LeakyReLU-15         [-1, 64, 256, 256]               0
+           Conv2d-16         [-1, 64, 256, 256]          36,928
+          conv_dr-17         [-1, 64, 256, 256]               0
+      BatchNorm2d-18         [-1, 64, 256, 256]             128
+        LeakyReLU-19         [-1, 64, 256, 256]               0
+           Conv2d-20         [-1, 64, 256, 256]          36,928
+          conv_dr-21         [-1, 64, 256, 256]               0
+      BatchNorm2d-22         [-1, 64, 256, 256]             128
+             ReLU-23         [-1, 64, 256, 256]               0
+           Conv2d-24         [-1, 64, 256, 256]          36,928
+          conv_dr-25         [-1, 64, 256, 256]               0
+      BatchNorm2d-26         [-1, 64, 256, 256]             128
+             ReLU-27         [-1, 64, 256, 256]               0
+           Conv2d-28         [-1, 64, 256, 256]          36,928
+          conv_dr-29         [-1, 64, 256, 256]               0
+      BatchNorm2d-30         [-1, 64, 256, 256]             128
+             ReLU-31         [-1, 64, 256, 256]               0
+           Conv2d-32         [-1, 64, 256, 256]          36,928
+          conv_dr-33         [-1, 64, 256, 256]               0
+      BatchNorm2d-34         [-1, 64, 256, 256]             128
+             ReLU-35         [-1, 64, 256, 256]               0
+           Conv2d-36          [-1, 3, 256, 256]           1,731
+             conv-37          [-1, 3, 256, 256]               0
+             ReLU-38          [-1, 3, 256, 256]               0
+           Lambda-39          [-1, 3, 256, 256]               0
+             Tanh-40          [-1, 3, 256, 256]               0
+================================================================
+Total params: 299,971
+Trainable params: 299,971
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.75
+Forward/backward pass size (MB): 1127.50
+Params size (MB): 1.14
+Estimated Total Size (MB): 1129.39
+----------------------------------------------------------------
+```
--- model.py
+++ model.py
@@ -1,68 +1,101 @@
-from this import d
-from tkinter import N
-from turtle import forward
-import torch
 import torch.nn as nn
+from torch import divide
 
 
-def conv3x3(in_planes, out_planes, dilation_rate=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, dilation=dilation_rate)
+class conv(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        return x
+    
+
+class conv_dr(nn.Module):
+    def __init__(self, in_c, out_c, dilation_rate=1, padding=1):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_c, out_c, kernel_size=3, padding=padding, dilation=dilation_rate)
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        return x
+
+
+class Lambda(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        return x + 1e-7
+
 
 class DespeckleFilter(nn.Module):
-    def __init__(self, input_shape) -> None:
+    def __init__(self, in_c) -> None:
         super(DespeckleFilter, self).__init__()
         
-        self.conv3x3_1 = conv3x3(input_shape)
-        self.batch_norm = nn.BatchNorm2d(input_shape)
-        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
-        
-        self.conv3x3_2 = conv3x3(input_shape, dilation_rate=1)
-        self.conv3x3_3 = conv3x3(input_shape, dilation_rate=2)
-        self.conv3x3_4 = conv3x3(input_shape, dilation_rate=3)
-        self.conv3x3_5 = conv3x3(input_shape, dilation_rate=4)
-        
+        self.conv_1 = conv(in_c=in_c, out_c=64)
+        self.conv_2 = conv(in_c=64, out_c=3)
+        self.leaky_rely = nn.LeakyReLU(negative_slope=0.2)
+        self.b_n = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
+        self.conv_dr_1 = conv_dr(64, 64, 1, 1)
+        self.conv_dr_2 = conv_dr(64, 64, 2, 2)
+        self.conv_dr_3 = conv_dr(64, 64, 3, 3)
+        self.conv_dr_4 = conv_dr(64, 64, 4, 4)
+        self.lambda_layer = Lambda()
+        self.tanh = nn.Tanh()
         
-    def forward(self, x):
-        x = x.size()
-        x = self.conv3x3_1(x) # filters = 64
-        x = self.leaky_relu(x)
+    def forward(self, inputs):
+        input_layer = inputs
+        x = self.conv_1(inputs)
+        x = self.leaky_rely(x)
         
-        x = self.conv3x3_2(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
+        x = self.conv_dr_1(x)
+        x = self.b_n(x)
+        x = self.leaky_rely(x)
         
-        x = self.conv3x3_3(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
+        x = self.conv_dr_2(x)
+        x = self.b_n(x)
+        x = self.leaky_rely(x)
         
-        x = self.conv3x3_4(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
+        x = self.conv_dr_3(x)
+        x = self.b_n(x)
+        x = self.leaky_rely(x)
         
-        x = self.conv3x3_5(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
+        x = self.conv_dr_4(x)
+        x = self.b_n(x)
+        x = self.leaky_rely(x)
         
-        x = self.conv3x3_5(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
-        
-        x = self.conv3x3_4(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
-        
-        x = self.conv3x3_3(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
-        
-        x = self.conv3x3_2(x) # filters = 64
-        x = self.batch_norm(x)
-        x = self.leaky_relu(x)
-        
-        x = self.conv3x3_5(x) # filter = 1
+        x = self.conv_dr_4(x)
+        x = self.b_n(x)
         x = self.relu(x)
         
-        out = x
-        return out
+        x = self.conv_dr_3(x)
+        x = self.b_n(x)
+        x = self.relu(x)
         
+        x = self.conv_dr_2(x)
+        x = self.b_n(x)
+        x = self.relu(x)
+        
+        x = self.conv_dr_1(x)
+        x = self.b_n(x)
+        x = self.relu(x)       
+        
+        x = self.conv_2(x)
+        x = self.relu(x)
+        
+        x = self.lambda_layer(x)
+        x = divide(input_layer, x)
+        x = self.tanh(x)
+        
+        return x
+    
+
+if __name__ == "__main__":
+    from torchsummary import summary
+    model = DespeckleFilter(3)
+    summary(model, input_size=(3, 256, 256))
